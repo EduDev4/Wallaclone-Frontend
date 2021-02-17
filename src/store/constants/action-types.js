02d542abdefee2b3d6bc92b21cc9634415fdfd06
Ivan Chinea <chitenavi@gmail.com>
2021-02-17 22:16:16 +0000

    refact: add selectors, todos, preloadedState
    - Add selectors to store
    - Add preloadedState, if we have user info in localStorage
    - Comments for team

--- src/store/constants/action-types.js
+++ src/store/constants/action-types.js
@@ -1,3 +1,8 @@
 // TODO: Creados como modelo, reemplazar por los nuevos actions
+// Peticiones al API, request, success, failure. Mejor para interfaz de usuario,
+// spinners, errores, notificaciones.
+
+// export const API_REQUEST = 'api/REQUEST';
+// export const API_FAILURE = 'api/FAILURE';
 export const ADD_USER = 'ADD_USER';
 export const REMOVE_USER = 'REMOVE_USER';
