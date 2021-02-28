82aefc35536f27157e4531041bf54265928918a7
Andrea G. Aristegui <andrearistegui@gmail.com>
2021-02-28 20:19:18 +0100

    header y footer inicial

--- src/api/client.js
+++ src/api/client.js
@@ -1,10 +1,22 @@
 import axios from 'axios';
+import i18n from 'i18next';
 
 const { REACT_APP_API_BASE_URL: baseURL } = process.env;
 
 const client = axios.create({
   baseURL,
 });
+
+// Interceptor request for language selection
+// client.interceptors.request.use(req => {
+//   if (req.url.includes('?')) {
+//     req.url += `&lang=${i18n.language}`;
+//   } else {
+//     req.url += `?lang=${i18n.language}`;
+//   }
+//
+//   return req;
+// });
 
 export const setAuthorizationHeader = token => {
   client.defaults.headers.common.Authorization = `Bearer ${token}`;
