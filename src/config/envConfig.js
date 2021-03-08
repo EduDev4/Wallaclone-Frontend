81c2bb25f1850f193013b2d0b8b81793bbf7b7e0
Ivan Chinea <chitenavi@gmail.com>
2021-03-08 12:48:29 +0000

    Fix: public url for static files

--- src/config/envConfig.js
+++ src/config/envConfig.js
@@ -5,9 +5,4 @@ export const getApiBaseUrl = () => {
   return process.env.REACT_APP_API_BASE_URL_DEV;
 };
 
-export const getPublicUrl = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return process.env.REACT_APP_PUBLIC_URL_PROD;
-  }
-  return process.env.REACT_APP_PUBLIC_URL_DEV;
-};
+export const getPublicUrl = () => process.env.PUBLIC_URL;
